894ac902e7c1be2f821ff02abe41b76e9c09629c
ersadishla <ersadishlahuddin@gmail.com>
2021-04-29 23:18:09 +0700

    change default database config to sqlsrv

--- src/config/module.database.php
+++ src/config/module.database.php
@@ -1,23 +1,14 @@
 <?php
 
 return [
-    'driver' => 'mysql',
+    'driver' => env('DB_DRIVER', 'sqlsrv'),
     'url' => env('DATABASE_URL'),
-    'host' => env('DB_HOST', '127.0.0.1'),
-    'port' => env('DB_PORT', '3306'),
+    'host' => env('DB_HOST', 'localhost'),
+    'port' => env('DB_PORT', '1433'),
     'database' => env('DB_DATABASE', 'forge'),
     'username' => env('DB_USERNAME', 'forge'),
     'password' => env('DB_PASSWORD', ''),
-    'unix_socket' => env('DB_SOCKET', ''),
-    'charset' => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci',
+    'charset' => 'utf8',
     'prefix' => '',
     'prefix_indexes' => true,
-    'strict' => true,
-    'engine' => null,
-    'options' => extension_loaded('pdo_mysql') ? array_filter(
-        [
-            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-        ]
-    ) : [],
 ];
